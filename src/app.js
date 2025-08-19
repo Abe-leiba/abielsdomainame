34f3edea9527c564fc4d8c6125fade6eaeca1767
Abe-leiba <abel_con@yahoo.com>
2025-08-19 21:50:20 +0000

    finished domain name extensions

--- src/app.js
+++ src/app.js
@@ -6,6 +6,30 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+    let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let extention = ['.net', '.org', '.gov'];
+  
+
+
+  for (let i =0; i <pronoun.length; ++i) {
+    for (let j = 0; j < adj.length; ++j) {
+         for (let k = 0; k < noun.length; ++k) {
+           for (let l = 0; l < extention.length; ++l) {
+             console.log (pronoun[i] + adj[j] + noun[k] + extention[l] + '.com');
+             }
+         }   
+    }
+}
+
+
+  
+
+
+
+
+  
+
   console.log("Hello Rigo from the console!");
 };
