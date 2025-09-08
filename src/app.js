5d184728f5d40de1c1eeb2437482fe3027f7ba30
Abe-leiba <abel_con@yahoo.com>
2025-09-08 17:28:31 +0000

    domain name generator .com fix still want to come back and display in browser using query selector when i have a chance

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ window.onload = function() {
     let pronoun = ['the', 'our'];
   let adj = ['great', 'big'];
   let noun = ['jogger', 'racoon'];
-  let extention = ['.net', '.org', '.gov'];
+  let extention = ['.net', '.org', '.gov','.com'];
   
 
 
@@ -17,19 +17,11 @@ window.onload = function() {
     for (let j = 0; j < adj.length; ++j) {
          for (let k = 0; k < noun.length; ++k) {
            for (let l = 0; l < extention.length; ++l) {
-             console.log (pronoun[i] + adj[j] + noun[k] + extention[l] + '.com');
+             console.log (pronoun[i] + adj[j] + noun[k] + extention[l]);
              }
          }   
     }
 }
-
-
-  
-
-
-
-
-  
 
   console.log("Hello Rigo from the console!");
 };
